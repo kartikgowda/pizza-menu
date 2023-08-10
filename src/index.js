b10f10c328d4a96ab44186b35d574c7c12a190a1
Kartik <kartik.gowda55@gmail.com>
2023-08-10 12:36:39 +1000

    Add Base Structure

--- src/index.js
+++ src/index.js
@@ -2,7 +2,44 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 function App() {
-  return <h1>Hello React !</h1>;
+  return (
+    <div>
+      <Header />
+      <Menu />
+      <Footer />
+    </div>
+  );
+}
+
+function Pizza() {
+  return (
+    <div>
+      <img src="/pizzas/spinaci.jpg" alt="Pizza Spinaci"></img>
+      <h2>Pizza Spinaci</h2>
+      <p>Tomato, mozarella, spinach, and ricotta cheese </p>
+    </div>
+  );
+}
+
+function Header() {
+  return <h1>React Pizza Co.</h1>;
+}
+
+function Menu() {
+  return (
+    <div>
+      <h2>Our Menu</h2>
+      <Pizza />
+      <Pizza />
+      <Pizza />
+    </div>
+  );
+}
+
+function Footer() {
+  return (
+    <footer>{new Date().toLocaleTimeString()}.We're currently OPEN</footer>
+  );
 }
 
 // @ts-ignore
